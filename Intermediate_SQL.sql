a08769f110bfc1b623452c2885aad49ee41e412c
Meet8844 <meetlathiya301@gmail.com>
2025-02-10 15:44:49 +0000

    String Functions

--- Intermediate_SQL.sql
+++ Intermediate_SQL.sql
@@ -255,9 +255,47 @@ WHERE Salary > 70000
 ORDER BY first_name, last_name
 ;
 
+
+-- String Functions
+SELECT LENGTH('Skyfall')
+;
+
+SELECT first_name, LENGTH(first_name)
+FROM employee_demographics
+ORDER BY 2
+;
+
+SELECT UPPER('sky');
+SELECT LOWER ('SKY');
+
+SELECT first_name, UPPER(first_name)
+FROM employee_demographics
+;
+
+select TRIM('         sky         ');
+select RTRIM('         sky         ');
+select LTRIM('         sky         ');
+
+SELECT first_name,
+LEFT (first_name, 4),
+RIGHT (first_name, 4),
+SUBSTRING(first_name, 3, 2),
+birth_date,
+SUBSTRING(birth_date, 6,2) AS birth_month
+FROM employee_demographics;
+
+
+SELECT first_name, REPLACE(first_name, 'a', 'z')
+FROM employee_demographics;
+
+SELECT LOCATE('x', 'Alexander');
+
+SELECT first_name, LOCATE('An', first_name)
+FROM employee_demographics;
     
-    
-    
+SELECT first_name, last_name,
+CONCAT(first_name,' ', last_name) AS full_name
+FROM employee_demographics;
     
     
     
