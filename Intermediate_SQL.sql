1cd498cd403025c367fafc96fe1991db5426e213
Meet8844 <meetlathiya301@gmail.com>
2025-02-20 18:44:09 +0000

    Stored Procedure

--- Intermediate_SQL.sql
+++ Intermediate_SQL.sql
@@ -492,7 +492,55 @@ FROM employee_salary
 WHERE salary >= 50000;
 
 SELECT *
-FROM salary_over_50k;
+FROM salary_over_50k; 
+
+
+-- Stored Procedures
+SELECT *
+FROM employee_salary
+WHERE salary >= 50000;
+
+CREATE PROCEDURE large_salaries()
+SELECT*
+FROM employee_salary
+WHERE salary >= 50000;
+
+CALL large_salaries();
+
+
+DELIMITER $$
+CREATE PROCEDURE large_salaries3()
+BEGIN
+	SELECT*
+	FROM employee_salary
+	WHERE salary >= 50000;
+	SELECT*
+	FROM employee_salary
+	WHERE salary >= 10000;
+END $$
+DELIMITER ;
+
+CALL large_salaries3()
+
+
+DELIMITER $$
+CREATE PROCEDURE large_salaries4(p_employee_id INT)
+BEGIN
+	SELECT salarys
+	FROM employee_salary
+	WHERE employee_id = p_employee_id
+    ;
+	END $$
+DELIMITER ;
+
+CALL large_salary4(1)
+
+
+
+
+
+
+    
 
 
 
